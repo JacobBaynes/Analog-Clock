c1a5c71e828dc1bd44868db10c4c1171d84b3d76
Jake B <jacob.baynes@icloud.com>
2019-10-18 10:19:54 -0400

    Added time zone selection functionality to two buttons

--- script.js
+++ script.js
@@ -8,48 +8,62 @@ const PACIFIC = document.querySelector("#pacific");
 // const EASTERN = document.querySelector("#easter");
 const MY = document.querySelector("#my");
 
-var date = new Date();
-console.log(date);
-setClock(date);
+var interval
+
+let start = new Date();
+setClock(start);
+
 function setClock(date) {
     let hr = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
+    
     console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+    console.log(date);
+    
+    hrPosition = (hr*360/12)+(min*(360/60)/12);
+    minPosition = (min*360/60)+(sec*(360/60)/60);
+    secPosition = sec*360/60;
+    
+    console.log("hrPos: " + hrPosition + " minPos: " + minPosition + " secPos: " + secPosition)
+    
+    interval = setInterval(runTheClock, 1000);
 
-    let hrPosition = (hr*360/12)+(min*(360/60)/12);
-    let minPosition = (min*360/60)+(sec*(360/60)/60);
-    let secPosition = sec*360/60;
-
-    function runTheClock() {
-
-        hrPosition = hrPosition+(3/360);
-        minPosition = minPosition+(6/60);
-        secPosition = secPosition+6;
-        
-        HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-        MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-        SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-    }
-
-    var interval = setInterval(runTheClock, 1000);
+    console.log(interval);
 }
 
+function runTheClock() {
+    
+    hrPosition = hrPosition+(3/360);
+    minPosition = minPosition+(6/60);
+    secPosition = secPosition+6;
+    
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    
+    // console.log("hrPos: " + hrPosition + " minPos: " + minPosition + " secPos: " + secPosition)
+    
+}
+
+
 function myTime() {
+    clearInterval(interval);
+    interval = null;
     let date = new Date();
     setClock(date);
 }
 
 function pacificTime() {
-
-    let localTime = date.getTime();
-    let localOffset = date.getTimezoneOffset() * 60000;
+    clearInterval(interval);
+    interval = null;
+    let d = new Date();
+    let localTime = d.getTime();
+    let localOffset = d.getTimezoneOffset() * 60000;
     let utc = localTime + localOffset;
     let offset = 8
     let pacTime = utc - (3600000 * offset);
     let newDate = new Date(pacTime);
-    console.log(newDate);
-
     setClock(newDate);
 
     // let hr = date.getHours();
